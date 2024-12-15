09238f62ef6c0dba83733dda906c3a7d05ddb29c
Fabrício Silva <fabricio85silva@hotmail.com>
2024-12-15 14:40:37 -0300

    add: troca do menu pelo slider no modo garcom

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,13 +1,17 @@
 import { allWishIcon, preparationicon, wishReadyIcon } from "@/utils/Icons";
 
-export function Slider() {
+interface SliderProps {
+    className?: string
+}
+
+export function Slider(props: SliderProps) {
   const listStyle =
     " flex cursor-pointer justify-center items-center gap-2 bg-orange-main text-white font-semibold px-5 py-2 rounded-lg active:scale-110 tooltip md:tooltip-bottom";
 
    const text = 'hidden md:block' 
 
   return (
-    <ul className="flex h-10 mt-5 justify-center items-center gap-6 w-full">
+    <ul className={`flex h-10 mt-5 justify-center items-center gap-6 w-full ${props.className}`}>
       <li className={`${listStyle} `} data-tip='Todos'>
         <span className="text-3xl md:text-lg">{allWishIcon}</span>
         <p className={text}>TODOS</p>
